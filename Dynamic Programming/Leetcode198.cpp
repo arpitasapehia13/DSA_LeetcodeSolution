5180696b0fff582a931b8b426c9d1d5f30f51264
arpitasapehia13 <arpitasapehia@gmail.com>
2024-11-03 12:45:14 +0530

    Added Solution Leetcode70.cpp

---
+++ Dynamic Programming/Leetcode198.cpp
@@ -0,0 +1,21 @@
+//HOUSE ROBBER BY MEMO
+
+class Solution {
+public:
+
+    int t[101];
+    int solution(vector<int>& nums, int i , int n){
+        if(i>=n) return 0;
+        if(t[i] != -1) return t[i];
+
+        int steal = nums[i] + solution(nums,i+2,n);
+        int skip = solution(nums,i+1,n);
+
+        return t[i] = max(steal,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solution(nums,0,n);
+    }
+};
