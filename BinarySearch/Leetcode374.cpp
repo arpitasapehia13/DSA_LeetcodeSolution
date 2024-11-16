12196bc7ca59f57fd89913216ddec17c219f10ae
Arpita Sapehia <103949456+arpitasapehia13@users.noreply.github.com>
2024-11-16 12:42:40 +0530

    Update Leetcode374.cpp

--- BinarySearch/Leetcode374.cpp
+++ BinarySearch/Leetcode374.cpp
@@ -1,13 +1,5 @@
 // GUESS HIGHER OR LOWER
 
-/**
- * Forward declaration of guess API.
- * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			      1 if num is lower than the picked number
- *               otherwise return 0
- * int guess(int num);
- */
 
 class Solution
 {
