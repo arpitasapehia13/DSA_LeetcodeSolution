47ab96338931100db71e391268f9fc4249e3b75a
arpitasapehia13 <arpitasapehia@gmail.com>
2024-12-10 03:21:59 +0530

    Updated

--- SortingAlgorithm/InsertionSort.cpp
+++ SortingAlgorithm/InsertionSort.cpp
@@ -1,31 +1,38 @@
-#include <bits/stdc++.h>
-#include <iostream>   
-#include <vector>
+#include <iostream>
 using namespace std;
 
 void insertion_sort(int arr[], int n) {
-    for (int i = 0; i <= n - 1; i++) {
-        int j = i;
-        while (j > 0 && arr[j - 1] > arr[j]) {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
+    for (int i = 1; i < n; i++) { // Start from the second element
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements that are greater than 'key' one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
             j--;
         }
+        arr[j + 1] = key; // Place the key at its correct position
     }
 }
 
 int main() {
     int n;
+    cout << "Enter the number of elements: ";
     cin >> n;
+
     int arr[n];
+    cout << "Enter the elements: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i]; 
+        cin >> arr[i];
     }
 
     insertion_sort(arr, n);
+
+    cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
