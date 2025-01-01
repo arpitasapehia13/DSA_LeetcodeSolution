84495726a51ac192a0d0c299276629263f7a3a17
arpitasapehia13 <arpitasapehia@gmail.com>
2025-01-01 19:46:36 +0530

    Added

--- SortingAlgorithm/InsertionSort.cpp
+++ SortingAlgorithm/InsertionSort.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 void insertion_sort(int arr[], int n) {
-    for (int i = 1; i < n; i++) { // Start from the second element
+    for (int i = 1; i < n; i++) { 
         int key = arr[i];
         int j = i - 1;
 
-        // Move elements that are greater than 'key' one position ahead
+        
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j + 1] = key; // Place the key at its correct position
+        arr[j + 1] = key; 
     }
 }
 
